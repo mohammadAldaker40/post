f475b380b1d399a3a39c69b2809d589800dba664
mohammad aldaker <mohammaddakerlovebasem@gmail.com>
2025-04-21 14:42:19 +0300

    edit css

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,19 +1,23 @@
 export default function PostCard({children, title, tags = []}) {
 
     return (
-        <div className="post-card tags">
-            <h1>{title}</h1>
-            <p>{children}</p>
-            <h2>Tags</h2>
-            
-                {tags.map(
-                    (tag) => {
-                        return (
-                            <li key={tag}>{tag}</li>
-                        )
-                    }
-                )}
-            
+        <div className="post-card">
+            <div className="post-card-header">
+            <h2>{title}</h2>
+            </div>
+            <div className="post-card-content">
+                <p>{children}</p>
+            </div>
+            <h3>Tags</h3>
+            <div className="post-card-tags">
+            {tags.map(
+                (tag) => {
+                    return (
+                         <li className="tags" key={tag}>{tag}</li>
+                    )
+                }
+            )}
+            </div>
         </div>
     )
 }
