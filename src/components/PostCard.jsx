bef07b7c512182fbb770668edb5b90c6a83fff76
mohammad aldaker <mohammaddakerlovebasem@gmail.com>
2025-04-22 16:02:36 +0300

    fix another bug

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,11 @@
-export default function PostCard({children, title, tags = []}) {
+
+export default function PostCard({children, title, tags = []  }) {
 
     return (
         <div className="post-card">
-            <div className="post-card-header">
-            <h2>{title}</h2>
-            </div>
+                
             <div className="post-card-content">
-                <p>{children}</p>
+                {children}
             </div>
             <h3>Tags</h3>
             <div className="post-card-tags">
